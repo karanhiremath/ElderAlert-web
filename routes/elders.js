1f9d2186c9f7d0a60fb36a5f5d40c9af0341ff10
Dhruv Maheshwari <dhwari@gmail.com>
2015-04-05 11:29:00 -0400

    get geofences working

--- routes/elders.js
+++ routes/elders.js
@@ -135,32 +135,29 @@ router.get('/:username', function(req,res,next){
 router.post('/:username/updateGeofence', function(req, res){
     var username = req.params.username;
     console.log(username);
-    var query = new parse.Query(parse.User);
-    query.equalTo("username", username);
-    query.first({
-      success: function(user) {
-        //use
-        console.log(user);
-        var geofence = Geofence.spawn(parseFloat(req.body.latitude), parseFloat(req.body.longitude), parseFloat(req.body.radius));
-        user.set("geofence", geofence);
-        user.set("name", "test2");
-        user.save(null, {
-            success: function(user) {
-            // The object was saved successfully.
-                console.log(user);
-            },
-            error: function(user, error) {
-            console.log(error);
+    var elderIdQuery = new parse.Query(Elder);
+    elderIdQuery.equalTo("user.username",username);
+    elderIdQuery.find({
+        success: function(elders) {
+            if(elders.length > 0){
+                var elderId = elders[0].id;
+                var elderObjQuery = new parse.Query(Elder);
+                elderObjQuery.get(elderId,{
+                    success: function(elder) {
+                        var geofence = Geofence.spawn(parseFloat(req.body.latitude), parseFloat(req.body.longitude), parseFloat(req.body.radius));
+                        elder.set("geofence", geofence);
+                        elder.save();
+                        res.send(200);
+                    },
+                      error: function(user, error) {
+                        console.log(error);
+                        res.send(500);
+                        // The object was not retrieved successfully.
+                        // error is a Parse.Error with an error code and message.
+                    }
+                });
             }
-        });
-      },
-      error: function(user, error) {
-
-        console.log(error);
-        res.send(500);
-        // The object was not retrieved successfully.
-        // error is a Parse.Error with an error code and message.
-      }
+        }
     });
 });
 
