1b30eda67dd414c61a7c1e2f570a745785c41abf
Nina Lu <ninajlu@gmail.com>
2015-05-02 18:06:36 -0400

    added mandrill

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.stormpath = {};
 config.parse= {};
-
+config.mandrill = {};
 config.redis = {};
 config.web = {};
 config.session = {};
@@ -15,6 +15,8 @@ config.web.baseUrl = 'http://elderalert.herokuapp.com';
 config.parse.appId = 'cyBvaz32pZq15Njaw8NQ1ujsaF8MoAglFlxSNIWO';
 config.parse.jsKey = 'nA13ZsrYne3vBn1CNiFjAvRbuLYzde0OtbENNkxv';
 config.parse.masterKey = 'RnOezvcQNzDpuh4CX0kCenZ5nYRmHQa0w3ozFb4e';
+config.mandrill.apiUser = 'ninajlu@gmail.com';
+config.mandrill.apiKey = 'RzKFmwsc9CV7RgrQ-Gjrag';
 
 //session secret
 config.session.secret = process.env.EXPRESS_SECRET || "fa2d5588dbef6144f7aa8688577bf89c2ff0686a85d4cbffb0d59779d13ee9e0a5e2c2d9792212c6";
